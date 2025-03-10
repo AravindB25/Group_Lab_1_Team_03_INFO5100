ef4b9dec4f7f55b7c9ac6af9675d8154fafc78a1
AravindB25 <balaji.ara@northeastern.edu>
2025-03-10 19:01:36 -0400

    TeamGreeting_Aravind_Balaji

---
+++ Group_Lab_1_Team_03_Lab6_INFO6150_001564773/src/group_lab_1_team_03_lab6_info6150_001564773/Aravind_Balaji.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group_lab_1_team_03_lab6_info6150_001564773;
+
+/**
+ *
+ * @author aravindbalaji
+ */
+public class Aravind_Balaji {
+    public static void main(String[] args) {
+        System.out.println("Hello from Aravind Balaji!");
+    }
+}
