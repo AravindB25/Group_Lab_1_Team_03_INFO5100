3a2575c25249c33e7c5c0666a52176f85723425a
TanyaBansal28 <bansal.t@northeastern.edu>
2025-03-10 19:10:49 -0400

    TeamGreeting_Tanya_Bansal

---
+++ Group_Lab_1_Team_03_INFO6150-main/Group_Lab_1_Team_03_Lab6_INFO6150_001564773/src/group_lab_1_team_03_lab6_info6150_001564773/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package group_lab_1_team_03_lab6_info6150_001564773;
+
+/**
+ *
+ * @author aravindbalaji
+ */
+public class Main {
+    public static void main(String[] args) {
+        Aravind_Balaji.main(args);
+        Tanya_Bansal.main(args);
+    }
+}
+
+
+    /**
+     * @param args the command line arguments
+     */
+    
+        // TODO code application logic here
+    
+    
